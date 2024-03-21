513be0a4516090580e0298131ab68efb03afa15c
sqWeeky <140751166+sqWeeky@users.noreply.github.com>
2024-03-21 08:14:45 +0500

    ghxjm

--- Assets/Scripts/HealthBar/HealthView.cs
+++ Assets/Scripts/HealthBar/HealthView.cs
@@ -2,12 +2,7 @@ using UnityEngine;
 
 public class HealthView : MonoBehaviour
 {
-    protected Health Health;
-
-    private void Awake()
-    {
-        Health = FindObjectOfType<Health>();
-    }
+    [SerializeField] protected Health Health;
 
     private void OnEnable()
         => Health.HealthChanged += DisplayAmount;
